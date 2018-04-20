2661f9d49c40ede1c6f49cfff310c2f7b3f6db73
lazyls <lazyls@yeah.net>
2018-04-20 18:49:43 +0800

    增加EqualityComparerFactory

---
+++ ls/Common.Utility/EqualityComparerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ls.Common.Utility
+{
+    public class EqualityComparerFactory<T> : EqualityComparer<T>
+    {
+        private Func<T, T, bool> comparer;
+
+        public override bool Equals(T x, T y)
+        {
+            var k = comparer(x, y);
+            return comparer(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return 1;
+        }
+
+        public EqualityComparerFactory(Func<T, T, bool> comparer)
+        {
+            this.comparer = comparer;
+        }
+    }
+}
