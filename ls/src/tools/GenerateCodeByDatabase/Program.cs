1e0e644432b336fd8c9e0f6f5a32f8111819f5aa
lazyls <lazyls@yeah.net>
2018-07-11 15:09:32 +0800

    todo..

--- ls/src/tools/GenerateCodeByDatabase/Program.cs
+++ ls/src/tools/GenerateCodeByDatabase/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace GenerateCodeByDatabase
 {
@@ -9,6 +10,8 @@ namespace GenerateCodeByDatabase
     {
         static void Main(string[] args)
         {
+            var a = ConfigurationManager.ConnectionStrings["CanadaComputersERP"].ConnectionString;
+            Console.WriteLine(a);
         }
     }
 }
